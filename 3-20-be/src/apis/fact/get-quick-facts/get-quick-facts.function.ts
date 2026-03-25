f14388dc1724ac9e205657d6cecc31fe084affa5
karan-salunkhe-tc <karan.salunkhe@thecontrast.in>
2026-03-25 17:05:47 +0530

    change random facts

--- 3-20-be/src/apis/fact/get-quick-facts/get-quick-facts.function.ts
+++ 3-20-be/src/apis/fact/get-quick-facts/get-quick-facts.function.ts
@@ -1,17 +1,15 @@
 import { Error_Interface } from "@config/interfaces/error.interface"
 import { get_quick_facts_function_params, get_quick_facts_function_return } from "./get-quick-facts.interface"
-import { Transaction } from "sequelize"
+import { Transaction, literal } from "sequelize"
 import { QuickFact } from "@src/models/quick-fact.model";
-import { build_query_options } from "@config/common/generic-build-query";
 
 let get_quick_facts_function = async (data: get_quick_facts_function_params, transaction: Transaction): Promise<get_quick_facts_function_return | Error_Interface> => {
     try {
-        const { pagination, order, filter } = build_query_options({ data, sort_fields: ["fact", "source"], search_fields: ["fact", "source"] });
-
         let quick_facts = await QuickFact.findAndCountAll({
-            where: filter, order,
+            order: literal('RANDOM()'),
             attributes: ['id', 'fact', 'source', 'createdAt', 'updatedAt'],
-            ...pagination, transaction, raw: true, nest: true
+            limit: 1,
+            transaction, raw: true, nest: true
         })
 
         if (quick_facts.count == 0) {
