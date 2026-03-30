25d9bdb394456e7f6d715120994c9d25e89002a6
karan-salunkhe-tc <karan.salunkhe@thecontrast.in>
2026-03-30 12:21:35 +0530

    zod changes

--- 3-20-be/src/apis/activity/get-user-activity/get-user-activity.query.schema.ts
+++ 3-20-be/src/apis/activity/get-user-activity/get-user-activity.query.schema.ts
@@ -4,7 +4,6 @@ import { z } from "zod";
 extendZodWithOpenApi(z)
 
 export let get_user_activity_query_schema = {
-    filter: z.enum(['Week', 'Month']).optional().openapi({ example: 'Week' }),
-    page: z.number().int().min(1).optional().openapi({ example: 1 }),
-    limit: z.number().int().min(1).max(50).optional().openapi({ example: 10 }),
+    page: z.coerce.number().int().min(1).optional().openapi({ example: 1 }),
+    limit: z.coerce.number().int().min(1).max(50).optional().openapi({ example: 10 }),
 };
