809aba20e34dcf506bb8f76e3e2be7268059e21b
shreekaudare <shreeram.kaudare@thecontrast.in>
2026-04-08 10:29:38 +0530

    add landing page

--- 3-20-way-fe/src/app/app.routes.ts
+++ 3-20-way-fe/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { App } from './app';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingPageComponent },
+    {
+        path: 'pages', component: App, children: [
+            { path: '', component: LandingPageComponent, },
+            { path: 'landing-page', component: LandingPageComponent, },
+        ]
+    }
+];
