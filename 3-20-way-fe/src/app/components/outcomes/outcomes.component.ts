a08dc06e601f2c64fad9f506fd0037b60a272fb1
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-25 14:42:30 +0530

    creat new componet outcomes

---
+++ 3-20-way-fe/src/app/components/outcomes/outcomes.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-outcomes',
+  imports: [],
+  templateUrl: './outcomes.component.html',
+  styleUrl: './outcomes.component.css'
+})
+export class OutcomesComponent {
+
+}
