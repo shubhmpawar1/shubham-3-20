ed3881040259b4b6b049f2830e1288a97f223878
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-25 22:12:29 +0530

    create new footer component

---
+++ 3-20-way-fe/src/app/components/new-footer/new-footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-footer',
+  imports: [],
+  templateUrl: './new-footer.component.html',
+  styleUrl: './new-footer.component.css'
+})
+export class NewFooterComponent {
+
+}
