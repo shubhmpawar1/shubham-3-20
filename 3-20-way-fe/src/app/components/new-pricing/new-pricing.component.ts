da2214b34a0c4601e5e914f32afc8ea41d321ec0
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-25 12:58:29 +0530

    add pricing componet

---
+++ 3-20-way-fe/src/app/components/new-pricing/new-pricing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-pricing',
+  imports: [],
+  templateUrl: './new-pricing.component.html',
+  styleUrl: './new-pricing.component.css'
+})
+export class NewPricingComponent {
+
+}
