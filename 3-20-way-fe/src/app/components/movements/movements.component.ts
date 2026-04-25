398695422ac589e3ebb4855551864430ad7da1d8
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-25 21:41:45 +0530

    create movement componet

---
+++ 3-20-way-fe/src/app/components/movements/movements.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movements',
+  imports: [],
+  templateUrl: './movements.component.html',
+  styleUrl: './movements.component.css'
+})
+export class MovementsComponent {
+
+}
