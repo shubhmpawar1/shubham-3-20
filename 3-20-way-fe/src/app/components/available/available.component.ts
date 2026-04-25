8e3d087fbdc1a066f30b7fdd95416c070af7f2c1
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-25 20:48:20 +0530

    created avaliable section

---
+++ 3-20-way-fe/src/app/components/available/available.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-available',
+  imports: [],
+  templateUrl: './available.component.html',
+  styleUrl: './available.component.css'
+})
+export class AvailableComponent {
+
+}
