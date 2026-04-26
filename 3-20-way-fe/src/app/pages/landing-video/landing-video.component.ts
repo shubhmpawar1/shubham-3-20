e34644e304d837fde9e8299c617efb709fcc3ae7
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-26 16:04:10 +0530

    add aauto video

--- 3-20-way-fe/src/app/pages/landing-video/landing-video.component.ts
+++ 3-20-way-fe/src/app/pages/landing-video/landing-video.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild, AfterViewInit, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,10 +9,38 @@ import { Router } from '@angular/router';
   templateUrl: './landing-video.component.html',
   styleUrl: './landing-video.component.css'
 })
-export class LandingVideoComponent {
-  constructor(private router: Router) {}
+export class LandingVideoComponent implements AfterViewInit {
+  isMuted = true;
+  @ViewChild('bgVideo') videoElement!: ElementRef<HTMLVideoElement>;
+
+  constructor(
+    private router: Router,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {}
+
+  ngAfterViewInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.playVideo();
+    }
+  }
+
+  playVideo() {
+    if (this.videoElement && isPlatformBrowser(this.platformId)) {
+      this.videoElement.nativeElement.play().catch(error => {
+        console.log('Autoplay was prevented, waiting for user interaction:', error);
+      });
+    }
+  }
 
   skipVideo() {
     this.router.navigate(['/landing-page']);
+  }
+
+  toggleMute(video: HTMLVideoElement) {
+    this.isMuted = !this.isMuted;
+    video.muted = this.isMuted;
+    if (video.paused) {
+      video.play();
+    }
   }
 }
