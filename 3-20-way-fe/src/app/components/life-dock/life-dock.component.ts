bac8a0add7a4d431149245e555804c1d78c0a188
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-28 13:00:19 +0530

    fix icon

--- 3-20-way-fe/src/app/components/life-dock/life-dock.component.ts
+++ 3-20-way-fe/src/app/components/life-dock/life-dock.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-life-dock',
@@ -10,4 +11,11 @@ import { CommonModule } from '@angular/common';
 })
 export class LifeDockComponent {
   @Input() life_dock_data: any;
+
+  constructor(private sanitizer: DomSanitizer) { }
+
+  getSafeIcon(iconPath: string): SafeHtml {
+    const svg = `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" aria-hidden="true" class="size-6 text-white">${iconPath}</svg>`;
+    return this.sanitizer.bypassSecurityTrustHtml(svg);
+  }
 }
