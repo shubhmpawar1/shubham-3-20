7abb23581e63db20907cba69d3bbff66bdf1ad41
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-28 11:02:17 +0530

    add copyright page style

--- 3-20-way-fe/src/app/pages/copyright-notice/copyright-notice.component.ts
+++ 3-20-way-fe/src/app/pages/copyright-notice/copyright-notice.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NewFooterComponent } from '../../components/new-footer/new-footer.component';
+import { NewFooterService } from '../../services/new-footer.service';
+import { LandingPageService } from '../../services/landing-page.service';
 
 @Component({
   selector: 'app-copyright-notice',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule, NewFooterComponent],
   templateUrl: './copyright-notice.component.html',
   styleUrl: './copyright-notice.component.css'
 })
 export class CopyrightNoticeComponent {
-
+  constructor(
+    public nfs: NewFooterService,
+    public ls: LandingPageService
+  ) { }
 }
