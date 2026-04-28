b1987426c9f247bfa6295f8cb6c11cea49efde57
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-28 10:54:35 +0530

    add term page

---
+++ 3-20-way-fe/src/app/pages/terms-condition/terms-condition.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-terms-condition',
+  imports: [],
+  templateUrl: './terms-condition.component.html',
+  styleUrl: './terms-condition.component.css'
+})
+export class TermsConditionComponent {
+
+}
