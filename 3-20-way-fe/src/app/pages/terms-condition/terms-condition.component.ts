df0b92779d2fa84497e4de488919c9784be14c9f
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-28 11:14:26 +0530

    fix copyright page stytle

--- 3-20-way-fe/src/app/pages/terms-condition/terms-condition.component.ts
+++ 3-20-way-fe/src/app/pages/terms-condition/terms-condition.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { NewFooterComponent } from '../../components/new-footer/new-footer.component';
+import { NewFooterService } from '../../services/new-footer.service';
 
 @Component({
   selector: 'app-terms-condition',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule, NewFooterComponent],
   templateUrl: './terms-condition.component.html',
   styleUrl: './terms-condition.component.css'
 })
 export class TermsConditionComponent {
-
+  constructor(public nfs: NewFooterService) { }
 }
