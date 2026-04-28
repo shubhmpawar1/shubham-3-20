2684f85addcd0d3d8d30611cd151531290b5b924
Shubham Pawar <shubham.pawar@thecontrast.in>
2026-04-28 11:27:10 +0530

    add new page data olicy

---
+++ 3-20-way-fe/src/app/pages/data-policy/data-policy.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-policy',
+  imports: [],
+  templateUrl: './data-policy.component.html',
+  styleUrl: './data-policy.component.css'
+})
+export class DataPolicyComponent {
+
+}
